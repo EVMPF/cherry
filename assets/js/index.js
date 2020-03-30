dbdc0b9efc814356fc11f894f7ddf890d6106888
EVMPF <a3214561444@gmail.com>
2020-03-30 21:10:33 +0800

    Cherry v0.21 nav repaire

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,10 @@ $(function () {
     $(window).scroll(function () {
         if (window.scrollY > 100) {          /* 要滑動到選單的距離 */
             $('#nav').addClass('navFixed');   /* 幫選單加上固定效果 */
+            console.log("add");
         } else {
             $('#nav').removeClass('navFixed'); /* 移除選單固定效果 */
+            console.log("rm");
         }
     });
 });
